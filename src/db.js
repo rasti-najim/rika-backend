ac470a744f32ff7ba404bfb9d0c763e4658911a3
Raste Aldawoodi <ra244@duke.edu>
2024-01-27 23:19:51 -0500

    updated db

--- src/db.js
+++ src/db.js
@@ -35,6 +35,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // For testing purposes. For production, use proper SSL configuration.
+  },
 });
 
 module.exports = { client, pool, redisClient };
